341bfec4e5422997abdf9feb29514e2b03048989
Jaden <624133845@qq.com>
2019-06-26 14:46:43 +0800

    .

--- src/main.js
+++ src/main.js
@@ -4,13 +4,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import 'babel-polyfill';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import store from './store';
 import api from './common/api';
 import './common/utils/rem'; //自适应rem函数
 // import rem from './common/utils/rem';
 
 Vue.prototype.$api = api;
+
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 /* eslint-disable no-new */
 new Vue({
